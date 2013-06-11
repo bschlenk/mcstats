ff0e1d3de8f975202a781c34cb037caf960b6728
Brian Schlenker <bschlenk@umich.edu>
2013-06-11 17:14:30 -0400

    created class wrapper around log file, broke playtime_compiler

--- playtime_compiler.py
+++ playtime_compiler.py
@@ -4,13 +4,16 @@ import time
 import os
 import re
 
-from util import get_players, filter_warnings
+from util import get_players, MCLogFile, timestamp_re
+from util import timestamp2time
 
-def get_stamp(filter, input):
+login_strings = ['joined the game', r'.*?logged in with.*?$']
+logoff_strings = [r'.*?lost connection$', ]
+
+def get_stamp(match_object):
 	"""Returns a tuple of player, timestamp based on minecraft log timestamp"""
-	result = filter.match(input)
-	player = result.group(2)
-	timestamp = time.strptime(result.group(1), "%Y-%m-%d %H:%M:%S")
+	player = match_object.group(2)
+	timestamp = timestamp2time(match_object.group(1))
 	return (player, timestamp)
 
 def get_lists():
@@ -20,15 +23,23 @@ def get_lists():
 
 	logged_in = []
 	logged_out = []
-	stamp_filter = re.compile("(.*?) \[INFO\] ({0}).*".format(player_regex))
+	filter_base = r'({0}) \[INFO\] ({1}) (%s)'.format(timestamp_re, player_regex)
+	login_filter = re.compile(filter_base % '|'.join(login_strings))
+	logoff_filter = re.compile(filter_base % '|'.join(logoff_strings))
 
 
-	with open("server_filtered.log", 'r') as f:
-		for line in f:
-			if "disconnect." in line:
-				logged_out.append(get_stamp(stamp_filter, line))
-			if "logged in with" in line:
-				logged_in.append(get_stamp(stamp_filter, line))
+	mclog = MCLogFile('server.log')
+	for line in mclog.info_no_convo():
+		print line
+		result = login_filter.match(line)
+		if result:
+			logged_in.append(get_stamp(result))
+			continue
+		result = logoff_filter.match(line)
+		if result:
+			logged_out.append(get_stamp(result))
+			continue
+		print line,
 	
 	return logged_in, logged_out
 				
@@ -41,7 +52,7 @@ def main():
 	for x in get_players():
 		playtimes[x] = 0
 
-	filter_warnings()
+	remove_warnings()
 	logged_in, logged_out = get_lists()
 	for x in logged_in:
 		found = False
@@ -57,4 +68,10 @@ def main():
 
 	print playtimes
 
-main()
+#main()
+#x, y = get_lists()
+
+
+mclog = MCLogFile('server.log')
+for line in mclog.info():
+	print line
--- util.py
+++ util.py
@@ -1,27 +1,74 @@
 #!/usr/bin/python
 
 import os
+import time
+import re
 
-server_dir = '/home/brian/minecraft'
-player_dir = 'world/players'
+_server_dir = '/home/brian/minecraft'
+_player_dir = 'world/players'
+_log_file = os.path.join(_server_dir, 'server.log')
 
 # used for regex requiring timestamp parsing
 timestamp_re = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}"
 
 def timestamp2time(stamp):
 	"""Convert minecraft log timestamp to time object."""
-	return time.strptime(result.group(1), "%Y-%m-%d %H:%M:%S")
+	return time.strptime(stamp, "%Y-%m-%d %H:%M:%S")
 
 def get_players():
 	"""Return a list of players on the server, based on player.dat files."""
-	player_files = os.listdir(os.path.join(server_dir, 'world/players'))
+	player_files = os.listdir(os.path.join(_server_dir, _player_dir))
 	players = []
 	for player in player_files:
 		players.append(os.path.splitext(player)[0])
 	return players
 
+class MCLogFile():
+	_warn_strs = ['[WARNING]', '[SEVERE]']
+	_contents = []
+
+	def __init__(self, path=_log_file):
+		self._log_file = path
+		with open(self._log_file) as f:
+			self._contents = [line.strip() for line in f]
+		#self.no_warnings = []
+		#self.warnings = []
+		#for line in self._contents:
+			#if not '[WARNING]' in line:
+				#self._contents_no_warning.append(line)
+			#else:
+				#self.warnings.append(line)
+	
+	def __iter__(self):
+		return iter(self._contents)
+
+	def info(self):
+		for line in self._contents:
+			if '[INFO]' in line:
+				yield line
+
+	def info_no_convo(self):
+		pattern = re.compile(r'^%s \[INFO\] <.*?> .*?$' % (timestamp_re, ))
+		for line in self.info():
+			if pattern.match(line):
+				continue
+			else:
+				return line
+		
+
+	def no_warnings(self):
+		for line in self._contents:
+			if not any(word in line for word in self._warn_strs):
+				yield line
+
+	def warnings(self):
+		for line in self._contents:
+			if '[WARNING]' in line:
+				yield line
+
 def remove_warnings():
-	"""Remove warnings from the log file."""
+	"""Depricated - use MCLogFile.no_warnings instead
+	Remove warnings from the log file."""
 	with open("server.log", 'r') as f_in:
 		with open("server_filtered.log", 'w') as f_out:
 			for line in f_in:
